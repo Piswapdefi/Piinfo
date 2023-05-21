317e82dc70478383e6ec7185d6ec6189b7d4d266
Piswap <133732880+Piswapdefi@users.noreply.github.com>
2023-05-22 00:29:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Pi Swap");
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+// Định nghĩa các route cho ứng dụng
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('This is the About page.');
+});
+
+// Khởi động máy chủ và lắng nghe các kết nối đến cổng đã chỉ định
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
