db34c33db9cd66be47884fdd993829d00e7c16b1
Piswap <133732880+Piswapdefi@users.noreply.github.com>
2023-05-22 00:35:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
 
-// Định nghĩa các route cho ứng dụng
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get('/', async (req, res) => {
+  try {
+    // Gọi API từ trang web "https://piswap.onrender.com/api/data"
+    const response = await axios.get('https://piswap.onrender.com/api/data');
+    const data = response.data;
+
+    // Xử lý dữ liệu từ API và trả về kết quả cho client
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-app.get('/about', (req, res) => {
-  res.send('This is the About page.');
-});
-
-// Khởi động máy chủ và lắng nghe các kết nối đến cổng đã chỉ định
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
