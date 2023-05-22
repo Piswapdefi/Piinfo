74ce8fbc438773aaf483ec3dc30eb6705605af11
Piswap <133732880+Piswapdefi@users.noreply.github.com>
2023-05-22 12:38:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
 const axios = require('axios');
 
-const zt = axios.create({
-  baseURL: 'https://piswap.onrender.com/',
-});
+async function triggerDeploy() {
+  const deployHookURL = 'https://api.render.com/deploy/srv-chl50067avj2179k2kbg?key=0QR7G7oywtg'; // Thay thế bằng Deploy Hook URL của bạn
 
-zt.defaults.headers.common['Authorization'] = '';
-
-async function pR(t, e = {}, r = 'get', n) {
-  return new Promise(async (resolve, reject) => {
-    let headers = {};
-    if (n && n.state) {
-      headers['Authorization'] = `Bearer ${n.token}`;
-    }
-
-    try {
-      const response = await zt({
-        method: r,
-        url: t,
-        headers: headers,
-        data: e,
-      });
-      resolve(response.data);
-    } catch (error) {
-      const customError = new Error(error.message);
-      customError.info = error.response.data;
-      reject(customError);
-    }
-  });
+  try {
+    const response = await axios.post(deployHookURL);
+    console.log('Triggered deploy successfully');
+  } catch (error) {
+    console.error('Failed to trigger deploy:', error.message);
+  }
 }
 
-// Sử dụng hàm pR
-(async () => {
-  try {
-    const result = await pR('/api/endpoint', { key: 'value' }, 'post', { state: true, token: 'your_token' });
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
-})();
+// Gọi hàm triggerDeploy để kích hoạt yêu cầu POST
+triggerDeploy();
