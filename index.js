e0e10548a1cca42fabffb36dee399f92efad058e
Piswap <133732880+Piswapdefi@users.noreply.github.com>
2023-05-22 09:04:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
-const express = require('express');
-const app = express();
+// index.js
+const zt = require('axios');
 
-const allowedDomains = ['http://127.0.0.1:5503/', 'www.piswap.io'];
+zt.Axios = cc;
+zt.CanceledError = nl;
+// Các dòng mã khác...
 
-// Middleware kiểm tra tên miền được phép
-function checkAllowedDomain(req, res, next) {
-  const origin = req.headers.origin;
-  if (allowedDomains.includes(origin)) {
-    // Cho phép yêu cầu từ tên miền được xác thực
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  } else {
-    // Không cho phép yêu cầu từ tên miền khác
-    return res.status(403).send('Forbidden');
-  }
-  next();
+const hR = "https://piswap.onrender.com/";
+
+async function pR(t, e = {}, r = "get", n) {
+  // Các dòng mã của hàm pR...
 }
 
-// Sử dụng middleware cho tất cả các yêu cầu
-app.use(checkAllowedDomain);
+async function main() {
+  try {
+    const response = await pR("endpoint", { data: "example" });
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-// Định nghĩa endpoint "/api/data"
-app.get('/api/data', (req, res) => {
-  // Xử lý yêu cầu GET đến "/api/data"
-  // ...
-  res.send('Data response');
-});
-
-// Định nghĩa trang chủ
-app.get('/', (req, res) => {
-  res.send('Welcome to the homepage');
-});
-
-// Khởi động máy chủ
-app.listen(3000, () => {
-  console.log('Server is running on port 443');
-});
+main();
