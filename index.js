8c180801f8e4d37061000ec2b5994d588500b23e
Piswap <133732880+Piswapdefi@users.noreply.github.com>
2023-05-22 09:28:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const axios = require('axios');
 
 async function requestDataFromAPI() {
   try {
-    const response = await axios.get('https://piswap.onrender.com/api/endpoint');
+    const apiKey = 'rnd_J7JWn7Td7clVfbmg3nnxrCMMaGs0';
+    const headers = {
+      Authorization: `Bearer ${apiKey}`
+    };
+
+    const response = await axios.get('https://piswap.onrender.com/api/endpoint', { headers });
     console.log(response.data);
     // Xử lý dữ liệu tại đây
   } catch (error) {
