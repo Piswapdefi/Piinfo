2a760f942bd4c139ce931c6d6c0d66be7071911f
Piswap <133732880+Piswapdefi@users.noreply.github.com>
2023-05-22 09:33:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+const express = require('express');
 const axios = require('axios');
 
-async function requestDataFromAPI() {
+const app = express();
+
+app.get('/', async (req, res) => {
   try {
     const response = await axios.get('https://piswap.onrender.com/api/endpoint');
-    console.log(response.data);
+    const data = response.data;
     // Xử lý dữ liệu tại đây
+    res.send(data);
   } catch (error) {
     console.error(error);
+    res.status(500).send('Error');
   }
-}
+});
 
-requestDataFromAPI();
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
