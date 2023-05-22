90e2222ee2c4d547edbb6b7dc16def70c64ee256
Piswap <133732880+Piswapdefi@users.noreply.github.com>
2023-05-22 12:40:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const axios = require('axios');
-
 async function triggerDeploy() {
   const deployHookURL = 'https://api.render.com/deploy/srv-chl50067avj2179k2kbg?key=0QR7G7oywtg'; // Thay thế bằng Deploy Hook URL của bạn
 
   try {
-    const response = await axios.post(deployHookURL);
-    console.log('Triggered deploy successfully');
+    const response = await fetch(deployHookURL, { method: 'POST' });
+    if (response.ok) {
+      console.log('Triggered deploy successfully');
+    } else {
+      throw new Error('Failed to trigger deploy');
+    }
   } catch (error) {
     console.error('Failed to trigger deploy:', error.message);
   }
