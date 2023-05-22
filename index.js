406504ccaf18e3975992e828929e838e292333a1
Piswap <133732880+Piswapdefi@users.noreply.github.com>
2023-05-22 09:26:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function requestDataFromAPI() {
   try {
-    const response = await axios.get('https://piswap.onrender.com:443/api/endpoint');
+    const response = await axios.get('https://piswap.onrender.com/api/endpoint');
     console.log(response.data);
     // Xử lý dữ liệu tại đây
   } catch (error) {
