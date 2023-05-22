1dc0bcfe75f6527fa814dbd6d40178aaad068e99
Piswap <133732880+Piswapdefi@users.noreply.github.com>
2023-05-22 11:40:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+// Tạo proxy server để truy cập API Render từ máy khách
 app.use(
   '/',
   createProxyMiddleware({
@@ -11,6 +12,19 @@ app.use(
   })
 );
 
-app.listen(443, () => {
-  console.log('Proxy server is running on port 443');
+// Truy cập API Render từ máy chủ
+app.get('/api', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.render.com/deploy/srv-chl50067avj2179k2kbg?key=0QR7G7oywtg');
+    const data = response.data;
+    // Xử lý dữ liệu tại đây
+    res.send(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
