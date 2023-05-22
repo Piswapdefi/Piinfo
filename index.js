7900e7c1c1bc9a8b8d57d71e23b18f600c589275
Piswap <133732880+Piswapdefi@users.noreply.github.com>
2023-05-22 13:02:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', async (req, res) => {
   try {
-    const response = await axios.get('https://api.render.com/deploy/srv-chl50067avj2179k2kbg?key=0QR7G7oywtg');
+    const response = await axios.get('postgres://piswap:0O1WlVc97wJNPWcwva08W3vvapLrtCzV@dpg-chl4blm4dadfmsh0ujk0-a/piswap');
     const data = response.data;
     // Xử lý dữ liệu tại đây
     res.send(data);
