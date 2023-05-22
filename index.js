e4a760a3ba3d860f914fa0b4f80fd06d7727f44d
Piswap <133732880+Piswapdefi@users.noreply.github.com>
2023-05-22 09:57:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express');
-const axios = require('axios');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-app.get('/', async (req, res) => {
-  try {
-    const response = await axios.get('https://api.render.com/deploy/srv-chl50067avj2179k2kbg?key=0QR7G7oywtg');
-    const data = response.data;
-    // Xử lý dữ liệu tại đây
-    res.send(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error');
-  }
-});
+app.use(
+  '/api/render',
+  createProxyMiddleware({
+    target: 'https://api.render.com',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api/render': '/deploy/srv-chl50067avj2179k2kbg?key=0QR7G7oywtg',
+    },
+  })
+);
 
-app.listen(443, () => {
-  console.log('Server is running on port 443');
+app.use(
+  '/',
+  createProxyMiddleware({
+    target: 'https://piswap.onrender.com',
+    changeOrigin: true,
+  })
+);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
